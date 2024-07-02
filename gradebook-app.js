8393fda0966d48bd96d4cfa287bc0f5b9c74ae18
Bashir Umar Idris <bashtrust15@gmail.com>
2024-07-02 14:03:56 +0100

    Fix a bug

--- gradebook-app.js
+++ gradebook-app.js
@@ -52,13 +52,13 @@ console.log(hasPassingGrade(87));
 
 function studentMsg(totalScores, studentScore) {
   let sum = 0;
-  sum += totalScores;
+  sum = sum + studentScore;
   let classAverage = sum / totalScores.length;
 
   if (getGrade(studentScore) !== "F") {
-    return `Class average: ${classAverage}. "\n" Your grade: ${studentScore}. You passed the course.`
+    return `Class average: ${classAverage}. \nYour grade: ${studentScore}. You passed the course.`
   } else {
-    return `Class average: ${classAverage}. Your grade: ${studentScore}. You failed the course.`
+    return `Class average: ${classAverage}. \nYour grade: ${studentScore}. You failed the course.`
   }
 }
 console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37));
